8a98d19dcd24f1bf0c589fc3394c55945a94f66f
Cristian Ignacio Gambetta Barrios <nachobrc11@gmail.com>
2026-07-21 17:17:07 -0300

    Add files via upload

---
+++ Clase #12 Sentencias condicionales multiples.py
@@ -0,0 +1,21 @@
+print("Convertidor de números a letras")
+
+num_uno = int(input("Que numero deseas convertir?: "))
+
+if num_uno == 1:
+    print("El número es 'Uno'")
+elif num_uno == 2:
+    print("El número es 'Dos'")
+elif num_uno == 3:
+    print("El número es 'Tres'")
+elif num_uno == 4:
+    print("El número es 'Cuatro'")
+elif num_uno == 5:
+    print("El número es 'Cinco'")
+
+else: 
+    print("Número desconocido")
+
+print("FIN!")
+    
+    
